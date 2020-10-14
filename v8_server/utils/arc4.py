fb09646ef591f4244e9c78d190bdc45cf6601641
573dev <>
2020-10-14 08:54:35 -0500

    Make ARC key cleaner

--- v8_server/utils/arc4.py
+++ v8_server/utils/arc4.py
@@ -4,16 +4,9 @@ from Crypto.Hash import MD5
 
 class EamuseARC4(object):
     def __init__(self, eamuse_key):
-        # fmt: off
-        internal_key = bytearray([
-            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x69, 0xd7,
-            0x46, 0x27, 0xd9, 0x85, 0xee, 0x21, 0x87, 0x16,
-            0x15, 0x70, 0xd0, 0x8d, 0x93, 0xb1, 0x24, 0x55,
-            0x03, 0x5b, 0x6d, 0xf0, 0xd8, 0x20, 0x5d, 0xf5,
-        ])
-        # fmt: on
-
-        key = MD5.new(eamuse_key + internal_key[6:]).digest()
+        secret_key = 0x69D74627D985EE2187161570D08D93B12455035B6DF0D8205DF5
+        key_bytes = bytearray(secret_key.to_bytes(26, "big"))
+        key = MD5.new(eamuse_key + key_bytes).digest()
         self.arc = ARC4.new(key)
 
     def decrypt(self, data):
